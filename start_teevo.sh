691a0a884876c2cc595a7d6f0bac7a968c06cd19
zhenghx <16-zhenghx@users.noreply.gitlab.hlrpz.com>
2024-02-03 20:27:47 +0800

    update

--- start_teevo.sh
+++ start_teevo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate dbgpt_env && python pilot/server/dbgpt_server.py
+conda activate dbgpt_env && python dbgpt/app/dbgpt_server.py
